0f581a26e54a4ed7bb0ef1ad293dd2af4f2d784f
lukeskywlker19 <thelukeskywlker19@gmail.com>
2025-10-22 11:51:37 -0400

    Add 1.21.10 as a supported version to README.md and update the paper api dependency to use 1.21.10.
    Signed-off-by: lukeskywlker19 <thelukeskywlker19@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.9-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
     compileOnly("com.github.lukesky19:SkyLib:1.3.1.0")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
 
